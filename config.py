fd349b996ae766f7e39a05c7dbfe4021c430b33a
S2orange1288 <spidermans231288@gmail.com>
2024-11-22 08:55:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7259034553:AAHYWdbEpMxeJAkv_HH1QYOChdMLMvot13E")
+BOT_TOKEN=("7124159999:AAEJ92t6POU88C8BYCOnihH-PUQfiesM5eA")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("Luis_enriquei")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5184635355")
 
 
 
